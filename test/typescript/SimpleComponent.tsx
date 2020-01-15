87402fffb57081aef156cebaf3bec7502291d889
Mikolaj <mikolaj.a.marczak@gmail.com>
2020-01-15 18:00:20 +0100

    added component for tests

---
+++ test/typescript/SimpleComponent.tsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import React from 'react';
+
+
+const SimpleComponent = () => (
+  <div>
+    aaaa
+  </div>
+);
+
+export default SimpleComponent;
